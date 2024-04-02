4177dbd321ebbfa1c8d3889bf544a5b77d36d649
Phoe-nix-89 <sonkhoseth074@gmail.com>
2024-04-02 16:35:18 +0530

    Error Handling

---
+++ ex.js
@@ -0,0 +1,11 @@
+try{
+    console.log(a);
+    var a = 10;
+    // let a = 10;
+}
+catch{
+    console.log("Variable a is initialised with let hence it throws error");
+}
+finally{
+    console.log("Always");
+}
